07224e8e1dacc01d89bb98811fc87feac15a0ca9
Daniel T. Lee <danieltimlee@gmail.com>
2017-05-06 22:10:15 +0900

    Deploying Enviroment with certain context

--- flightplan.js
+++ flightplan.js
@@ -4,20 +4,21 @@ var cfg = {
   host: process.env.HOST,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
-  agent: process.env.SSH_AUTH_SOCK
+  agent: process.env.SSH_AUTH_SOCK,
+  webRoot: '/usr/local/www',
 };
 
 plan.target('rpi', cfg);
 
 plan.remote(function (remote) {
   remote.log('Moving to repo');
-  remote.exec('cd ~/git/rpi_ble');
+  remote.with('cd ~/git/rpi_ble', () => {
 
-  remote.log('Stashing all Files and pull New');
-  remote.exec('git stash');
-  remote.exec('git pull origin master');
+    remote.log('Stashing all Files and pull New');
+    remote.with('git stash && git pull origin master', () => {
 
-  remote.log('Npm install & run');
-  remote.exec('npm run build');
-  remote.exec('npm install');
+      remote.log('Npm install & run');
+      remote.exec('npm run build && npm install');
+    });
+  });
 });
