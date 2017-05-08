ed20125aa9b221d5c809d0f3c12b2f8e0d7cf30d
Daniel T. Lee <danieltimlee@gmail.com>
2017-05-08 16:49:16 +0900

    Build Script typo fix

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 NODE_VER="$(node -v)"
 
-if [ "v7*" == "${NODE_VER}" ]
+echo "Checking Node Version"
+echo "${NODE_VER}"
+
+if [ "v7*" != "${NODE_VER}" ]
 then
+    echo "Installing Node v7.*"
     curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
     sudo apt-get install -y nodejs
 fi
+
+echo "Node Check Completed"
 
 source ~/.bashrc
 sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
