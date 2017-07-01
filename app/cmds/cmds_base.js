cbb20eee3d23b34b68c5a2582143fd8098da0b98
Daniel T. Lee <danieltimlee@gmail.com>
2017-07-01 20:23:25 +0900

    Comment Added

--- app/cmds/cmds_base.js
+++ app/cmds/cmds_base.js
@@ -14,30 +14,30 @@ const dataSize = 20;
 const PktType = {
   SCAN_REQ: 1,
   SCAN_RES: 2,
-  SNSR_STAT_REQ: 3,
+  SNSR_STAT_REQ: 3, //Sensor Attach? Sent by Node
   SNSR_STAT_RES: 4,
-  SNSR_DATA_REQ: 5,
+  SNSR_DATA_REQ: 5, //Sensor Data Request from Hub. ex)Humid && Temp
   SNSR_DATA_RES: 6,
-  SNSR_ACT_REQ: 7,
+  SNSR_ACT_REQ: 7, //Sensor Activity from Node. ex) Button
   SNSR_ACT_RES: 8,
-  SNSR_CMD_REQ: 9,
+  SNSR_CMD_REQ: 9, //Sensor Command request from Hub. ex)Buzzor, IR
   SNSR_CMD_RES: 10,
 
-  NODE_STAT_REQ: 17, //
-  NODE_STAT_RES: 18, //
+  NODE_STAT_REQ: 17, //Node Battery Status.
+  NODE_STAT_RES: 18, //TODO: Not Implemented
   NODE_LED_REQ: 19,
   NODE_LED_RES: 20,
   NODE_BTN_PRESS_REQ: 21,
   NODE_BTN_PRESS_RES: 22,
 
-  NET_UPDATE_REQ: 101, //
-  NET_UPDATE_RES: 102,//
-  NET_ACK_REQ: 103, //
-  NET_ACK_RES: 104, //
-  NET_JOIN_REQ: 105, //
-  NET_JOIN_RES: 106, //
+  NET_UPDATE_REQ: 101, //Net ID Update.
+  NET_UPDATE_RES: 102, //
+  NET_ACK_REQ: 103, //Net Ack. If Node Alive?
+  NET_ACK_RES: 104, //TODO: Not Implemented
+  NET_JOIN_REQ: 105, //Net JOIN.b
+  NET_JOIN_RES: 106, //TODO: Not Implemented
 
-  SCAN_TGT_REQ: 238,
+  SCAN_TGT_REQ: 238, //IF TGT is connectable?
   SCAN_TGT_RES: 239
 };
 
